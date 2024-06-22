4bb5da99a7b838efbd04475fd04b89ff912681e6
Jin Park <jinpark1504@gmail.com>
2024-06-23 04:21:54 +0900

    Initial Commit

---
+++ NewsAppReview/NewsAppReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppReviewApp.swift
+//  NewsAppReview
+//
+//  Created by Jungjin Park on 2024-06-23.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
